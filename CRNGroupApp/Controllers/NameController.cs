e1999f69048c17370f1ddf6d87ad705d99136359
Daniel A Schroeder <danielschroederjr@gmail.com>
2017-06-06 11:42:20 -0400

    changed connection string in web.config, Adam D. did most of the work

--- CRNGroupApp/Controllers/NameController.cs
+++ CRNGroupApp/Controllers/NameController.cs
@@ -9,23 +9,23 @@ namespace CRNGroupApp.Controllers
 {
     public class NameController : Controller
     {
-        protected override void OnActionExecuted(ActionExecutedContext filterContext)
-        {
-            if (User != null)
-            {
-                var context = new ApplicationDbContext();
-                var username = User.Identity.Name;
+        //protected override void OnActionExecuted(ActionExecutedContext filterContext)
+        //{
+        //    if (User != null)
+        //    {
+        //        var context = new ApplicationDbContext();
+        //        var username = User.Identity.Name;
 
-                if (!string.IsNullOrEmpty(username))
-                {
-                    var user = context.Users.SingleOrDefault(u => u.UserName == username);
-                    string fullName = string.Concat(new string[] { user.FirstName, " ", user.LastName });
-                    ViewData.Add("FullName", fullName);
-                }
-            }
-            base.OnActionExecuted(filterContext);
+        //        if (!string.IsNullOrEmpty(username))
+        //        {
+        //            var user = context.Users.SingleOrDefault(u => u.UserName == username);
+        //            //string fullName = string.Concat(new string[] { user.FirstName, " ", user.LastName });
+        //            ViewData.Add("FullName", fullName);
+        //        }
+        //    }
+        //    base.OnActionExecuted(filterContext);
         }
-        public NameController()
-        { }
-    }
+        //public NameController()
+        //{ }
+    
 }
